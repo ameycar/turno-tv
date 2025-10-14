909c8d67706dbe250247e26982da6709a978cecd
ameycar <camilacostamejias@gmail.com>
2025-10-14 12:26:28 -0500

    firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,1 +1,21 @@
+// firebase-config.js (modular v9)
+// Pega este archivo en la raíz del proyecto
 
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-database.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAX2VYw2XVs6DGsw38rCFaSbk3VuUA60y4",
+  authDomain: "estado-pacientes.firebaseapp.com",
+  databaseURL: "https://estado-pacientes-default-rtdb.firebaseio.com",
+  projectId: "estado-pacientes",
+  storageBucket: "estado-pacientes.appspot.com",
+  messagingSenderId: "515522648971",
+  appId: "1:515522648971:web:d7b6e9cde4a7d36181ad8e"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
+
+export const auth = getAuth(app);
